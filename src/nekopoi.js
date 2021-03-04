af7365260a772c0e89bcc7b0d61cf0a94bc1fc16
キリスト教徒 <77716224+cychrisfds@users.noreply.github.com>
2021-03-04 09:56:27 -0300

    Update nekopoi.js

--- src/nekopoi.js
+++ src/nekopoi.js
@@ -1,9 +1,9 @@
 const nekopoi = () => { 
 	return `       
-Mau akses/nonton nekopoi tanpa download vpn? Klik link dibawah ini lalu scroll kebawah terus ketik setuju & hubungkan.
+Você quer acessar/assistir nekopoi sem baixar VPN? Clique no link abaixo e, em seguida, role para baixo e digite concordar e conectar.
 
 https://www.hidemyass-freeproxy.com/proxy/id-id/aHR0cHM6Ly9uZWtvcG9pLmNhcmUv
 
-*Selamat Menonton*`
+*Divirta-se assistindo*`
 }
 exports.nekopoi = nekopoi
